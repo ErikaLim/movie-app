9f156868206a4e27496d8b3326b3320d8981dc75
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-14 14:51:54 -0800

    completed

--- app/controllers/concerns/movies_controller.rb
+++ app/controllers/concerns/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   def index
+
   end
 
   def search
     title = params[:q]
     @response = RestClient.get "http://www.omdbapi.com/?s=#{title}"
-    @movies = JSON.parse(response.body)
+    @movies = JSON.parse(@response.body)["Search"]
   end
 
 end
