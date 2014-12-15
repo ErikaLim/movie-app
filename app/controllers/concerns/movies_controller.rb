19d3168c831aed14fa89654516ef3a06ee93bd21
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-15 08:53:36 -0800

    fixed bug

--- app/controllers/concerns/movies_controller.rb
+++ app/controllers/concerns/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
 
   def search
     title = params[:q]
-    @response = RestClient.get "http://www.omdbapi.com/?s=#{title}"
+    @response = RestClient.get "http://www.omdbapi.com/?s=#{title.gsub(" ", "+")}"
     @movies = JSON.parse(@response.body)["Search"]
   end
 
